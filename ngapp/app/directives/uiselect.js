7a8b79e71c037f5ccd8e305210437978d32f8f0f
Yogesh Surendran <carreraphp@gmail.com>
2015-06-30 00:28:34 +0530

    Create uiselect.js

---
+++ ngapp/app/directives/uiselect.js
@@ -0,0 +1,16 @@
+function uiselect() {
+  return {
+    restrict: 'EA',
+    replace: true,
+    transclude: true,
+    controller: 'UISelectController',
+    scope: {
+      list: '=',
+      value: '=ngModel'      
+    },
+    templateUrl: 'app/view/directives/uiselect.html',
+    link: function(scope, element, attrs, UISelectCtrl) {
+//       UISelectCtrl.addBar(scope, angular.element(element.children()[0]));
+    }
+  };
+}
