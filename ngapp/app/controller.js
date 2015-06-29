ac342b24c3397a3c5748485a773a5f1e224a6a9c
Yogesh Surendran <carreraphp@gmail.com>
2015-06-29 23:44:31 +0530

    Create controller.js

---
+++ ngapp/app/controller.js
@@ -0,0 +1,9 @@
+angular.module('JFC.controller', [])
+    .constant('progressConfig', {
+            animate: true,
+            max: 100
+    })
+    .controller("ApplicationController", ["$scope", "$http", "$location", "$timeout", "$rootScope","$localStorage","$sessionStorage", ApplicationController])
+    .controller("HomeController", ["$scope", HomeController])	
+    .controller("LoginController", ["$scope", "$route","$http","$localStorage", LoginController])
+    .controller("UISelectController", ["$scope", "$attrs", "progressConfig", UISelectController]);
