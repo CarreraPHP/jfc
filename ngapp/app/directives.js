862b3333bbcd7fcee4f4575a2563451e416cb6c6
Yogesh Surendran <carreraphp@gmail.com>
2015-06-29 23:48:47 +0530

    Create directives.js

---
+++ ngapp/app/directives.js
@@ -0,0 +1,18 @@
+angular.module('JFC.directives', [])    
+    .directive('popup',function(){
+    	return {
+    	    restrict: 'EA',
+    	    replace: true,
+    	    transclude: true,
+    	    //controller: 'ProgressController',
+    	    scope: {
+    	      value: '=',
+    	      type: '='
+    	    },
+    	    templateUrl: 'app/view/directives/popup.html',
+    	    link: function(scope, element, attrs, popupCtrl) {
+    	    	//popupCtrl.addBar(scope, angular.element(element.children()[0]));
+    	    }
+    	  };
+    })
+    .directive('uiselect', uiselect);
