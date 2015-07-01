f757db393ec1c012bb5cfb100191657ca9acf532
Yogesh Surendran <carreraphp@gmail.com>
2015-07-01 22:24:05 +0530

    Removed Authentication
    Removed Authentication so that the page can be viewed by everyone

--- ngapp/application.js
+++ ngapp/application.js
@@ -35,18 +35,23 @@ var JFC = angular.module('JFC', [
                 $rootScope.application.showSubPageToolbarBtn = ($location.path().lastIndexOf('/') !== 0);
 
                 /* SKIP STATIC URL like Login, Register, ForgotPassword */			
-                if($location.path().indexOf('Login') == -1){
-                        if (!Auth.isLoggedIn()) {
-                                $rootScope.application.preventToolbar = true;
-                                $rootScope.application.preventTabbar = true;
-                                $location.path('/Login');
-                        } else {
-                                $rootScope.application.preventToolbar = false;
-                                $rootScope.application.preventTabbar = false;
-                        }
-                }else{
-                        $rootScope.application.preventToolbar = true;
-                        $rootScope.application.preventTabbar = true;
-                }
+                //                 if($location.path().indexOf('Login') == -1){
+//                         if (!Auth.isLoggedIn()) {
+//                                 $rootScope.application.preventToolbar = true;
+//                                 $rootScope.application.preventTabbar = true;
+//                                 $location.path('/Login');
+//                         } else {
+//                                 console.log("this page was called....");
+//                                 $rootScope.application.preventToolbar = false;
+//                                 $rootScope.application.preventTabbar = false;
+//                         }
+//                 }else{
+//                         $rootScope.application.preventToolbar = true;
+//                         $rootScope.application.preventTabbar = true;
+//                 }
+
+                $rootScope.application.preventToolbar = false;
+                $rootScope.application.preventTabbar = false;
+//                 $location.path('/Home');
         });
 }]);
