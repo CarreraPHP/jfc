2b577cddca00d0633fdef1e2f06c8e7370dedba7
Yogesh Surendran <carreraphp@gmail.com>
2015-07-01 23:58:20 +0530

    Added type property to the JSON chart data
    Added type property to the JSON chart data

--- ngapp/resources/js/chart3.js
+++ ngapp/resources/js/chart3.js
@@ -1,7 +1,8 @@
-var chart = [{
+var chart2 = [{
     id: 'ch0001',
-    name: 'User is getting slow response',
-    description: '',
+    name: 'Activity', 
+    type: 'Activity',
+    description: 'User is getting slow response',
     options: [{
         name: 'Always',
         impact: -1,
@@ -9,18 +10,19 @@ var chart = [{
     }]
 }, {
     id: 'ch0001-ch0002',
-    name: [
+    name: 'Activity', 
+    type: 'Activity',
+    description: [
         'Use Introscope <a href="https://google.com">to monitor the execution time</a> for queries, heap size, etc',
         'Use Introscope to monitor the execution time for queries, heap size, etc',
         'Use Introscope to monitor the execution time for queries, heap size, etc',
         'Use Introscope to monitor the execution time for queries, heap size, etc',
         'Use Introscope to monitor the execution time for queries, heap size, etc'
-    ].join(' '),
-    description: '',
+    ].join(' '),    
     options: [{
         name: 'Check',
         impact: -1,
-        charts: 'ch0001-ch0002-ch0003-ch0008'
+        charts: 'ch0001-ch0002-ch0003'
     }, {
         name: 'Always',
         charts: 'ch0001-ch0002-ch0004'
@@ -30,8 +32,9 @@ var chart = [{
     }]
 }, {
     id: 'ch0001-ch0002-ch0003',
-    name: 'Check whether the utilization of the server is high',
-    description: '',
+    name: 'Activity', 
+    type: 'Activity',
+    description: 'Check whether the utilization of the server is high',
     options: [{
         name: 'Always',
         impact: -1,
@@ -43,16 +46,18 @@ var chart = [{
     }]
 }, {
     id: 'ch0001-ch0002-ch0004',
-    name: 'Nothing Here',
-    description: '',
+    name: 'Activity', 
+    type: 'Activity',
+    description: 'Nothing Here',
     options: [{
         name: 'next',
         charts: 'ch0001-ch0002-ch0003-ch0008'
     }]
 }, {
     id: 'ch0001-ch0002-ch0003-ch0005',
-    name: 'Check whether the Query is taking more time',
-    description: '',
+    name: 'Activity', 
+    type: 'Activity',
+    description: 'Check whether the Query is taking more time',
     options: [{
         name: 'Always',
         impact: -1,
@@ -60,24 +65,27 @@ var chart = [{
     }]
 }, {
     id: 'ch0001-ch0002-ch0003-ch0005-ch0006',  
-    name: 'Try with different login id\'s and see if for all users getting slow response',
-    description: '',
+    name: 'Activity', 
+    type: 'Activity',
+    description: 'Try with different login id\'s and see if for all users getting slow response',
     options: [{
         name: 'Finish',
         charts: ''
     }]
 }, {
     id: 'ch0001-ch0002-ch0007',  
-    name: 'Check dynamic behaviour of the flow chart',
-    description: '',
+    name: 'Activity', 
+    type: 'Activity',
+    description: 'Check dynamic behaviour of the flow chart',
     options: [{
         name: 'Working',
         charts: 'ch0001-ch0002-ch0003-ch0005-ch0006'
     }]
 }, {
     id: 'ch0001-ch0002-ch0003-ch0008',
-    name: 'Check dynamic behaviour of the flow chart -2',
-    description: '',
+    name: 'Activity', 
+    type: 'Activity',
+    description: 'Check dynamic behaviour of the flow chart -2',
     options: [{
         name: 'Working',
         charts: ''
