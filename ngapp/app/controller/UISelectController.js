1c39a1f93ecf5044cb1351acf247a740ba3425e2
Yogesh Surendran <carreraphp@gmail.com>
2015-07-01 23:39:29 +0530

    Added placeholder for UI search component
    Added placeholder for UI search component

--- ngapp/app/controller/UISelectController.js
+++ ngapp/app/controller/UISelectController.js
@@ -2,8 +2,15 @@ function UISelectController($scope, $attrs, progressConfig) {
     var self = this;
 
     self.list = [];
+    console.log(arguments);
+    $scope.$watch($attrs.placeHolder, function(){
+        $scope.placeholder = $attrs.placeholder;    
+    });
     
     $scope.expand = false;
+    $scope.getPlaceHolder = function(){
+        return $attrs.placeHolder;
+    }
     $scope.expandList = function(){
         $scope.expand = !$scope.expand;
     }
