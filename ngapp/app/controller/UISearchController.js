1ed3b5b2d7d548be4750fe982559fcf81405384c
Yogesh Surendran <carreraphp@gmail.com>
2015-07-01 23:36:49 +0530

    Added UI search component controller
    Added UI search component controller

---
+++ ngapp/app/controller/UISearchController.js
@@ -0,0 +1,22 @@
+function UISearchController($scope, $attrs, progressConfig) {
+    var self = this;
+
+    self.list = [];
+    console.log(arguments);
+    $scope.$watch($attrs.placeHolder, function(){
+        $scope.placeholder = $attrs.placeholder;    
+    });
+    
+    
+    $scope.expand = false;
+    $scope.getPlaceHolder = function(){
+        return $attrs.placeHolder;
+    }
+    $scope.expandList = function(){
+        $scope.expand = !$scope.expand;
+    }
+    $scope.itemSelect = function(item){
+        $scope.value = item;
+        $scope.expand = false;
+    }
+}
