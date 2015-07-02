21ee39322d988723dd8099ca06cf16b769e13ef6
Yogesh Surendran <carreraphp@gmail.com>
2015-07-03 00:13:51 +0530

    Added Route for admin page
    Added Route for admin page

--- ngapp/app/router.js
+++ ngapp/app/router.js
@@ -6,6 +6,7 @@ function routerFn($routeProvider, $locationProvider) {
             .when('/Home', {templateUrl: 'app/view/Home.html', controller: 'HomeController'})
             .when('/Home/:subpage', {templateUrl: 'app/view/Home.html', controller: 'HomeController'})
             .when('/Login', {templateUrl: 'app/view/Login.html', controller: 'LoginController'})
+            .when('/Admin', {templateUrl: 'app/view/Admin.html', controller: 'AdminController'})
             .otherwise({
                     redirectTo:'/Home/select'
             });
