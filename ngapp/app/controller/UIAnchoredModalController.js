69bca3220adbb142fc58f78a4312e3fe42dc69b7
Yogesh Surendran <carreraphp@gmail.com>
2015-08-15 01:00:23 +0530

    Update UIAnchoredModalController.js

--- ngapp/app/controller/UIAnchoredModalController.js
+++ ngapp/app/controller/UIAnchoredModalController.js
@@ -1,25 +1,3 @@
 function UIAnchoredModalController($scope, $attrs, progressConfig, $timeout) {
     $scope.modal = {};
-    
-//    console.log($scope.overlayclick);
-//    $scope.$watch($attrs.show, function(){
-//        console.log($attrs, $attrs.show);
-//        $scope.modal.show = $attrs.show;
-//    });
-//    $scope.$watch($attrs.style, function(){
-//        console.log($attrs.style);
-//        $scope.modal.style = $attrs.style;
-//    });
-//    $scope.$watch($attrs.anchor, function(){
-//        console.log($attrs.anchor);
-//        $scope.modal.anchor = $attrs.anchor;
-//    });
-//    $scope.$watch($attrs.title, function(){
-//        console.log($attrs.title);
-//        $scope.modal.title = $attrs.title;
-//    });
-//    $scope.$watch($attrs.overlayclick, function(){
-//        console.log($attrs.overlayclick);
-//        $scope.modal.overlayclick = $attrs.overlayclick;
-//    });
 }
