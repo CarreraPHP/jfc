8b434c17ca1ee32edfe3fd548a3c65f4a4459206
CarreraPHP <carreraphp@gmail.com>
2015-08-30 13:31:46 +0530

    Added skipbeforenode filter
    Added skipbeforenode filter

--- ngapp/app/controller/UIChartOptionController.js
+++ ngapp/app/controller/UIChartOptionController.js
@@ -1,8 +1,9 @@
-function UIChartOptionController($scope, $attrs, progressConfig, $timeout) {
-    var self = this;    
+function UIChartOptionController($scope, $attrs, progressConfig, $timeout, $filter) {
+    var self = this;
     $scope.chartValueField = "id";
-    
-    
+
+    $scope.optionList = $filter('skipbeforenode')($scope.editor.chartList, $scope.subitem.charts) || [];
+
     $scope.chartChange = function(sItem, oItem){
         $timeout(function(){
             var a = angular.element(document.querySelector('#' + $scope.subitem.id + '-arrow-' + sItem)),
@@ -11,9 +12,9 @@ function UIChartOptionController($scope, $attrs, progressConfig, $timeout) {
 //                c = angular.element(document.querySelector('#' + sItem)),
 //                d = c.scope();
 //            console.log("%c arguments", "color:green;font-size:40px", a, b, c, d, $scope, arguments);
-        
+
             $scope.rearrangeOption(b.subitem, b.item, b.xIndex, b.yIndex, b.$index, b.yInit, b.editor.parsedList); //, d.xIndex);
-        }, 500); 
-        
+        }, 500);
+
     };
 }
