ada2291b9d8ca0e023706d042790718db14ef7fc
CarreraPHP <carreraphp@gmail.com>
2015-08-30 15:33:09 +0530

    removed display mode
    removed display mode

--- ngapp/app/controller/UIChartItemController.js
+++ ngapp/app/controller/UIChartItemController.js
@@ -1,26 +1,27 @@
 function UIChartItemController($scope, $attrs, progressConfig, $timeout) {
     var self = this;
     $scope.mode = 'edit'; // possible modes -> edit, display
-    $scope.typeList = ['Activity', 'Actor', 'End']; //There can be only one Begin Chart Item 'Begin', 
-    
-    $scope.$watch($scope.item.internal.mode, function(){
-        $scope.mode = $scope.item.internal.mode;
-    });
-    
+    $scope.typeList = ['Activity', 'Actor', 'End']; //There can be only one Begin Chart Item 'Begin',
+
+//    console.log("%c item", "color:red;font-size:20px;", $scope.item);
+//    $scope.$watch($scope.item.internal.mode, function(){
+//        $scope.mode = $scope.item.internal.mode;
+//    });
+
     $scope.$watch('item.name', function(){
         $scope.handleChange();
     });
-    
-    $scope.doEdit = function(){        
+
+    $scope.doEdit = function(){
         $scope.mode = $scope.item.internal.mode = 'edit';
     };
-    
+
     $scope.doSubmit = function(){
         $scope.mode = $scope.item.internal.mode = 'display';
     };
 
     $scope.handleChange = function(){};
-    
+
     $scope.showAllCustomiseTool = function(e, enabled){
         var scope = this,
             ct = angular.element(document.querySelectorAll('.customise-tool')),
